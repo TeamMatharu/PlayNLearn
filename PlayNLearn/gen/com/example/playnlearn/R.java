ff17badc2c7707a5a190ed08e4b486d5bdcbdc2d
rachitpandya <pandyarachit007@gmail.com>
2014-10-26 05:20:38 -0400

    First Commit With User Story : Selection of Profile/Guest

--- PlayNLearn/gen/com/example/playnlearn/R.java
+++ PlayNLearn/gen/com/example/playnlearn/R.java
@@ -10,11 +10,58 @@ package com.example.playnlearn;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int gamelogo=0x7f020000;
+        public static final int grdient=0x7f020001;
+        public static final int guest=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int profile=0x7f020005;
+    }
+    public static final class id {
+        public static final int action_settings=0x7f08000a;
+        public static final int imageView1=0x7f080001;
+        public static final int imgbtngst=0x7f080004;
+        public static final int imgbtnprof=0x7f080005;
+        public static final int scrollView1=0x7f080000;
+        public static final int tableRow1=0x7f080006;
+        public static final int tableRow2=0x7f080003;
+        public static final int tableRow3=0x7f080008;
+        public static final int tvGameName=0x7f080002;
+        public static final int txtCreateAccount=0x7f080007;
+        public static final int txtcrtusr=0x7f080009;
+    }
+    public static final class layout {
+        public static final int activity_new_profile=0x7f030000;
+        public static final int activity_profilequestion=0x7f030001;
+        public static final int activity_progressbar2=0x7f030002;
+        public static final int activity_selection=0x7f030003;
+    }
+    public static final class menu {
+        public static final int new_profile=0x7f070000;
+        public static final int profilequestion=0x7f070001;
+        public static final int progressbar2=0x7f070002;
+        public static final int selection=0x7f070003;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int action_settings=0x7f050003;
+        public static final int app_name=0x7f050000;
+        public static final int crtusr=0x7f050005;
+        public static final int hello_world=0x7f050002;
+        public static final int title_activity_new_profile=0x7f050006;
+        public static final int title_activity_progressbar2=0x7f050004;
+        public static final int title_activity_selection=0x7f050001;
     }
     public static final class style {
         /** 
@@ -38,10 +85,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f060001;
     }
 }
